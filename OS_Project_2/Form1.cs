d2ee36276c9e7f4f29dff201d3fbeaad84acaf9c
Dylan Vannatter <vannattd@mail.gvsu.edu>
2021-04-13 16:46:36 -0400

    Added Process class and generating a process.

--- OS_Project_2/Form1.cs
+++ OS_Project_2/Form1.cs
@@ -11,12 +11,27 @@ using System.Windows.Forms;
 namespace OS_Project_2
 {
 
+
+    public class Process
+    {
+        private readonly int size;
+        private readonly int name;
+        private readonly int startTime;
+
+        public Process(int size, int name, int startTime)
+        {
+            this.size = size;
+            this.name = name;
+            this.startTime = startTime;
+        }
+    }
 //Interface Process
 // size, name, start
     public partial class Form1 : Form
     {
         Timer t = new Timer();
         int time = 0;
+        int processNum = 1;
         public Form1()
         {
             InitializeComponent();
@@ -33,21 +48,41 @@ namespace OS_Project_2
 
         private void t_Tick(object sender, EventArgs e)
         {
+            Random rnd = new Random();
+            int processSize = rnd.Next(1, 11);
 
+            Process p = new Process(processSize, processNum, time);
             //Generate a process 
-      
+
 
             //Allocate a process
-            FirstFit(Process p);
-            BestFit(Process p);
-            WorstFit(Process p);
+            FirstFit(p);
+            BestFit(p);
+            WorstFit(p);
             time++;
+            processNum++;
             label12.Text = time.ToString();
 
         }
 
-       
 
-       
+        private void FirstFit(Process p)
+        {
+
         }
+
+        private void BestFit(Process p)
+        {
+
+        }
+
+        private void WorstFit(Process p)
+        {
+
+        }
+
+
+
+
+    }
 }
